4ee8895ec532ee2bd65a2c70ec66d393a4d6e72f
risshhabh <26rkshah@gmail.com>
2024-06-27 14:49:26 -0700

    implemented Bovine Genomics in python

---
+++ bronze/complete_search/bovine_genomics.py
@@ -0,0 +1,26 @@
+def old_contest(filename_no_ext):
+    import sys
+
+    sys.stdin = open(filename_no_ext + ".in", "r")
+    sys.stdout = open(filename_no_ext + ".out", "w")
+
+    return
+
+splint = lambda : list(map(int, input().split()))
+
+old_contest("cownomics")
+
+n_cows_2, len_genomes = splint()
+
+spotty_pos = 0
+
+spotted_per_gen = list(zip(*[input() for _ in range(n_cows_2)]))
+plain = list(zip(*[input() for _ in range(n_cows_2)]))
+
+for i in range(len_genomes):
+    if set(spotted_per_gen[i]) & set(plain[i]):
+        continue
+    else:
+        spotty_pos += 1
+
+print(spotty_pos)
